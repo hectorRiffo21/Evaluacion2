a94f17c874fbd57f41e8a5e837e6706983cfb218
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-08 14:24:58 -0300

    interfaz de el main

--- src/funciones_main.py
+++ src/funciones_main.py
@@ -1,3 +1,26 @@
+empleados = {}
+usuarios = {}
+
+def menu_principal():
+    print("---SISTEMA DE GESTION DE EMPLEADOS---")
+    print("1. Iniciar Sesion")
+    print("2. Registrar Empleado")
+    print("3. Mostrar Empleados")
+    print("4. Eliminar Empleado")
+    print("5. Salir")
+
+
+def menu_inventario():
+    print("Menu inventaario")
+    print("1.registrar producto")
+    print("2.mostrar productos")
+    print("3. actualizar stock")
+    print("4. buscar producto")
+    print("5. eliminar producto")
+    print("6. volver al menu principal")
+
+
+
 def agregar_producto():
     pass
 
@@ -12,3 +35,4 @@ def mostrar_productos():
 
 def buscar_productos():
     pass
+
