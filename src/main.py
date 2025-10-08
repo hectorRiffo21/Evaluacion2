dce2726a8bba282ffba165c49435aa1e36787b68
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-08 05:42:37 -0300

    correciones en la interfaz del main y funciones

--- src/main.py
+++ src/main.py
@@ -1,44 +1,129 @@
 import funciones_main as h
+from empleado import Empleado
+from producto import Producto
+from inventario import Inventario
+from gestor_empleado import GestorEmpleado
+
+def menu_principal():
+    print("---SISTEMA DE GESTION DE EMPLEADOS---")
+    print("1. Iniciar Sesion")
+    print("2. Registrar Empleado")
+    print("3. Mostrar Empleados")
+    print("4. Eliminar Empleado")
+    print("5. Salir")
+
+def menu_inventario():
+    print("Menu inventaario")
+    print("1.registrar producto")
+    print("2.mostrar productos")
+    print("3. actualizar stock")
+    print("4. buscar producto")
+    print("5. eliminar producto")
+    print("6. volver al menu principal")
 
 def main():
+    gestor = GestorEmpleado()
+    inventario = Inventario()
     while True:
-        h.menu_Usuarios()
-        try:
-            opcion_usuario = int(input("INGRESE OPCIÓN: "))
-            if opcion_usuario== 1:
-                #user = input("Ingrese nombre de usuario: ")
-                #clave = input("Ingrese password: ")
+        menu_principal()
+        opcion = input("seleccione una opcion : ").strip()
+        if opcion =="1":
+            usuario = input("Nombre de usuario : ")
+            clave = input("contraseña : ")
+            empleado = gestor.login(usuario,clave)
+            if empleado:
+                print("session iniciada")
+                print(f"\nBienvenido {empleado.get_nombre()} ({empleado.get_cargo_trabajo()})")
+                empleado.mostrar_informacion_empleado()
                 while True:
-                            h.menu_Principal()
+                    menu_inventario()
+                    opcion_inventario=input("seleccione opcion : ")
+                    if opcion_inventario=="1":
+                        nombre=input("nombre del producto: ")
+                        codigo=input("Codigo Unico del producto: ")
+                        categoria=input("Categoria: ")
+                        stock=input("Stock: ")
+                        stock_minimo=int(input("Stock minimo: "))
+                        stock_maximo=int(input("stock maximo: "))
+                        codigo_marca=input("codigo de marca: ")
+                        codigo_unidad=input("codigo de unidad : ")
+                        producto = Producto(nombre,codigo_marca,codigo_unidad,codigo,stock,stock_minimo,stock_maximo,categoria)
+                        try:
+                            inventario.agregar_producto(producto)
+                            print("producto registrado correctamente")
+                        except ValueError as e:
+                            print("error : ", e)
+                    elif opcion_inventario=="2":
+                        productos=inventario.listar_producto()
+                        if not productos:
+                            print("no hay productos registrados.")
+                        else:
+                            for prod in productos:
+                                print(prod)
+
+                    elif opcion_inventario=="3":
+                        codigo = input("codigo del producto: ")
+                        prod=inventario.buscar_producto(codigo)
+                        if prod:
+                            cantidad=int(input("cantidad a agregar (+) o retirar (-):"))
                             try:
-                                op = int(input("INGRESE OPCIÓN: "))
-                                if op == 1:
-                                    print("1")
-                                elif op == 2:
-                                    print("2")
-                                elif op == 3:
-                                  print("3")
-                                elif op == 4:
-                                   print("4")
-                                elif op == 5:
-                                    if input("¿DESEA SALIR [SI/NO]: ").lower() == "si":
-                                        break
-                                else:
-                                    print("OPCION FUERA DE RANGO !!!")
-                            except ValueError:
-                                print("INGRESE OPCION DENTRO DE RANGO!!")
-                    
-        
-            elif opcion_usuario == 2:
-                print("2")
-            elif opcion_usuario == 3:
-                if input("¿DESEA SALIR [SI/NO]: ").lower() == "si":
-                    break
-            else:
-                print("OPCION FUERA DE RANGO!!")
-        except ValueError:
-            print("INGRESE OPCION DENTRO DE RANGO!!")
+                                prod.actualizar_stock(cantidad)
+                                print(f"stock actualizado. nuevo stock: {prod.get_stock()}")
+                            except ValueError as e:
+                                print("error: ", e)
+                        else:
+                            print("producto no encontrado")
+                    elif opcion_inventario=="4":
+                        codigo = input("codigo del producto : ")
+                        prod = inventario.buscar_producto(codigo)
+                        if prod:
+                            print(prod)
+                        else:
+                            print("producto no encontrado")
+                    elif opcion_inventario=="5":
+                        codigo=input("codigo del producto: ")
+                        try:
+                            inventario.eliminar_producto(codigo)
+                            print("producto eliminado correctamente.")
+                        except ValueError as e:
+                            print("Error:",e)
 
+                    elif opcion_inventario=="6":
+                        break
 
-if __name__ == "__main__":
+                    else:
+                        print("opcion invalida")
+
+            input("enter para continuar")
+
+        elif opcion =="2":
+            print("Registrar nuevo empleado")
+            nombre= input("Nombre:")
+            apellido=input("Apellido:")
+            rut=input("Rut:")
+            celular=input("Celular:")
+            correo_electronico=input("Correo electronico:")
+            genero=input("Genero:")
+            nombre_usuario=input("Nombre de usuario: ")
+            clave=input("clave:")
+            cargo_trabajo=input("cargo:")
+
+            gestor.registrar_empleado(nombre,apellido,rut,celular,correo_electronico,genero,nombre_usuario,clave,cargo_trabajo)
+            pasar=input("presionar enter para continuar...")
+        elif opcion =="3":
+            print("Lista de empleados")
+            gestor.mostrar_empleados()
+            pasar=input("enter para continuar")
+        elif opcion=="4":
+            print("Eliminar empleado")
+            nombre_usuario = input("Nombre de usuario del empleado a eliminar : ")
+            gestor.eliminar_empleado(nombre_usuario)
+            pasar=input("Enter para continuar..")
+        elif opcion=="5":
+            print("Salienod del sistema")
+            break
+        else:
+            print("Opcion invalida, reintentar")
+
+if __name__ =="__main__":
     main()
