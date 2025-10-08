88f9c55d142f8b7fb86d7b3e635ebd0ad58640ee
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-08 04:29:58 -0300

    libreria bcrypt agregada, agregado de funcion para verificar clave encriptada, funcion para mostrar informacion de el empleado

--- src/empleado.py
+++ src/empleado.py
@@ -1,21 +1,33 @@
 from persona import Persona
+import bcrypt
 
 class Empleado(Persona):
-    def __init__(self,nombre,apellido,rut,celular,correo_electronico,genero, nombre_usuario,clave,cargo_trabajo):
+    def __init__(self,nombre,apellido,rut,celular,correo_electronico,genero, nombre_usuario,clave_hash,cargo_trabajo):
         super().__init__(nombre,apellido,rut,celular,correo_electronico,genero)
         self.__nombre_usuario = nombre_usuario
-        self.__clave = clave
+        self.__clave_hash = clave_hash
         self.__cargo_trabajo= cargo_trabajo
     def get_nombre_usuario(self):
         return self.__nombre_usuario
     def set_nombre_usuario(self,nombre_usuario):
         self.__nombre_usuario = nombre_usuario
-    def get_clave(self):
-        return self.__clave
-    def set_clave(self,clave):
-        self.__clave = clave
+    def get_clave_hash(self):
+        return self.__clave_hash
+    def set_clave_hash(self,clave_hash):
+        self.__clave_hash = clave_hash
     def get_cargo_trabajo(self):
         return self.__cargo_trabajo
     def set_cargo_trabajo(self,cargo_trabajo):
         self.__cargo_trabajo = cargo_trabajo
-        ##
+ 
+    def verificar_clave(self,clave,bcrypt):
+        return bcrypt.checkpw(clave.encode('utf-8'), self.__clave_hash)
+    
+    def mostrar_informacion_empleado(self):
+        super().mostrar_informacion()
+        print(f"Usuario : {self.__nombre_usuario}")
+        print(f"Cargo : {self.__cargo_trabajo}")
+
+
+
+
