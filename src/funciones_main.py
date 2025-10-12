fd15c5df40e71c96d21990820d62150d8e4d7349
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-12 16:49:23 -0300

    en proceso para desarrollar interfaz

--- src/funciones_main.py
+++ src/funciones_main.py
@@ -21,18 +21,5 @@ def menu_inventario():
 
 
 
-def agregar_producto():
-    pass
 
-def eliminar_producto():
-    pass
-
-
-def modificar_producto():
-    pass
-def mostrar_productos():
-    pass
-
-def buscar_productos():
-    pass
 
