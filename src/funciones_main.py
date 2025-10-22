d457306a76e553707c414ca4996c61b02c6a7515
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-22 00:32:41 -0300

    se agrego interfaz de menu movimiento donde se puede escoger la opcion que se necesite.

--- src/funciones_main.py
+++ src/funciones_main.py
@@ -2,7 +2,8 @@ empleados = {}
 usuarios = {}
 
 def menu_principal():
-    print("---SISTEMA DE GESTION DE EMPLEADOS---")
+    print("---------------------------------")
+    print("--SISTEMA DE GESTION DE EMPLEADOS--")
     print("1. Iniciar Sesion")
     print("2. Registrar Empleado")
     print("3. Mostrar Empleados")
@@ -11,14 +12,24 @@ def menu_principal():
 
 
 def menu_inventario():
-    print("Menu inventaario")
-    print("1.registrar producto")
-    print("2.mostrar productos")
-    print("3. actualizar stock")
-    print("4. buscar producto")
-    print("5. eliminar producto")
-    print("6. volver al menu principal")
+    print("---------------------------------")
+    print("--Menu Inventario--")
+    print("1. Registrar Producto")
+    print("2. Mostrar Productos")
+    print("3. Actualizar Stock")
+    print("4. Buscar Producto")
+    print("5. Eliminar Producto")
+    print("6. Mostrar Movimientos")
+    print("7. Mostrar Productos Por Debajo Del Stock Minimo")
+    print("8. Actualizar Limites De Stock")
+    print("9. Volver Al Menu Principal")
 
+def menu_movimientos():
+    print("\n--- Menú de Movimientos ---")
+    print("1. Ver todos los movimientos")
+    print("2. Filtrar por tipo")
+    print("3. Filtrar por usuario")
+    print("4. Volver al menú principal")
 
 
 
