b71ac763ec9a359aa505bf90bf0ebd0f3b428e3e
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-22 00:04:01 -0300

    se agrego precio como atributo, valor total para saber cuanto dinero se tiene en los productos, la funcion actualizar stock se le agrego validacion para no superar el stock maximo ingresado ni retirar mayor stock del disponible.

--- src/producto.py
+++ src/producto.py
@@ -1,34 +1,27 @@
 class Producto():
-    def __init__(self, nombre,codigo_marca,codigo_unidad,codigo,stock,stock_minimo,stock_maximo,categoria):
+    def __init__(self, nombre,codigo,stock,stock_minimo,stock_maximo,categoria,precio):
         self.__nombre = nombre
-        self.__codigo_marca = codigo_marca
-        self.__codigo_unidad = codigo_unidad
         self.__codigo = codigo
         self.__stock = stock
         self.__stock_minimo = stock_minimo
         self.__stock_maximo = stock_maximo
         self.__categoria = categoria
+        self.__precio = precio
+        self.__valor_total = self.calcular_valor_total()
 
     def get_nombre(self):
         return self.__nombre
     def set_nombre(self,nombre):
         self.__nombre = nombre
-    def get_codigo_marca(self):
-        return self.__codigo_marca
-    def set_codigo_marca(self,codigo_marca):
-        self.__codigo_marca = codigo_marca
-    def get_codigo_unidad(self):
-        return self.__codigo_unidad
-    def set_codigo_unidad(self,codigo_unidad):
-        self.__codigo_unidad = codigo_unidad
     def get_codigo(self):
         return self.__codigo
     def set_codigo(self,codigo):
         self.__codigo = codigo
     def get_stock(self):
         return self.__stock
-    def set_stock(self, stock):
-        self.__stock = stock
+    def set_stock(self, nuevo_stock):
+        self.__stock = nuevo_stock
+        self.__valor_total = self.calcular_valor_total()
     def get_stock_minimo(self):
         return self.__stock_minimo
     def set_stock_minimo(self,stock_minimo):
@@ -41,12 +34,30 @@ class Producto():
         return self.__categoria
     def set_categoria(self,categoria):
         self.__categoria = categoria
+    def get_precio(self):
+        return self.__precio
+    def set_precio(self,nuevo_precio):
+        self.__precio = nuevo_precio
+        self.__valor_total = self.calcular_valor_total()
 
-    def actualizar_stock(self, cantidad):
-        nuevo_stock = self.__stock + cantidad
-        if nuevo_stock < 0:
-            raise ValueError(f"No hay suficiente stock para retirar {abs(cantidad)} unidades.")
-        self.__stock = nuevo_stock
+    def get_valor_total(self):
+        return self.__valor_total
+    def calcular_valor_total(self):
+        return self.__precio*self.__stock
+
+    def actualizar_stock(self, cantidad, tipo):
+        if tipo=="ingreso":
+            if self.__stock + cantidad > self.__stock_maximo:
+                raise ValueError(f"No se puede ingresar {cantidad}. Stock máximo permitido: {self.__stock_maximo}. Stock actual: {self.__stock}")
+            self.__stock += cantidad
+        elif tipo=="retiro":
+            if cantidad>self.__stock:
+                raise ValueError(f"No hay suficiente stock para retirar {cantidad}. Stock actual: {self.__stock}")
+            self.__stock -= cantidad
+
+        else:
+            raise ValueError("Tipo de operación inválido. Use 'ingreso' o 'retiro'.")
+        self.__valor_total = self.calcular_valor_total()
 
     def datos_diccionario(self):
          return {
@@ -55,10 +66,12 @@ class Producto():
         "categoria": self.__categoria,
         "stock": self.__stock,
         "stock_minimo": self.__stock_minimo,
-        "stock_maximo": self.__stock_maximo
+        "stock_maximo": self.__stock_maximo,
+        "precio":self.__precio
     }
 
     def __str__(self):
         return (f"Producto: {self.__nombre} | Código: {self.__codigo} | "
                 f"Stock: {self.__stock} (Min: {self.__stock_minimo}, Max: {self.__stock_maximo}) | "
-                f"Categoría: {self.__categoria}")
+                f"Categoría: {self.__categoria} | "
+                f"Precio: {self.__precio}")
