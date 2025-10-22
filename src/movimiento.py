aab69c2769d20bfda88e820ca3e1c69bf6d47930
hectorRiffo21 <hector.riffo08@inacapmail.cl>
2025-10-22 00:05:36 -0300

    se tiene la clase movimiento con una libreria para registrar por fecha y hora los movimientos, aparte de quien fue el usuario responsable. y el monto total

--- src/movimiento.py
+++ src/movimiento.py
@@ -1,61 +1,65 @@
+import uuid
 from datetime import datetime
 
 class Movimiento():
     movimientos_registrados = {} 
-    contador=0
     #[]
 
-    def __init__(self,producto_codigo,tipo,cantidad,motivo,usuario_responsable):
-        Movimiento.contador +=1
-        self.__codigo_movimiento = Movimiento.contador 
-        self.__producto_codigo = producto_codigo
-        self.__tipo = tipo
-        self.__cantidad = cantidad
-        self.__motivo = motivo
-        self.__fecha = datetime.now()
+    def __init__(self,codigo_producto,usuario_responsable,tipo,cantidad,motivo,stock_antes,stock_despues, monto_total):
+        self.__id_movimiento = str(uuid.uuid4())
+        self.__codigo_producto = codigo_producto
         self.__usuario_responsable = usuario_responsable
-
-        Movimiento.movimientos_registrados[self.__codigo_movimiento]=self
-
-    def get_codigo_movimiento(self):
-        return self.__codigo_movimiento
-    def set_codigo_movimiento(self,codigo_movimiento):
-        self.__codigo_movimiento = codigo_movimiento
-    def get_producto_codigo(self):
-        return self.__producto_codigo
-    def set_producto_codigo(self,producto_codigo):
-        self.__producto_codigo = producto_codigo
-    def get_tipo(self):
-        return self.__tipo
-    def set_tipo(self,tipo):
         self.__tipo = tipo
-    def get_cantidad(self):
-        return self.__cantidad
-    def set_cantidad(self,cantidad):
-        self.__cantidad = cantidad
-    def get_motivo(self):
-        return self.__motivo
-    def set_motivo(self,motivo):
         self.__motivo = motivo
-    def get_fecha(self):
-        return self.__fecha
-    def set_fecha(self,fecha):
-        self.__fecha = fecha
+        self.__cantidad = cantidad
+        self.__fecha = datetime.now()
+        self.__stock_antes = stock_antes
+        self.__stock_despues = stock_despues
+        self.__monto_total = monto_total
+        
+
+    def get_id_movimiento(self):
+        return self.__id_movimiento
+    def get_codigo_producto(self):
+        return self.__codigo_producto
+    def set_codigo_producto(self,codigo_producto):
+        self.__codigo_producto = codigo_producto
     def get_usuario_responsable(self):
         return self.__usuario_responsable
     def set_usuario_responsable(self,usuario_responsable):
         self.__usuario_responsable = usuario_responsable
+    def get_tipo(self):
+        return self.__tipo
+    def set_tipo(self,tipo):
+        self.__tipo = tipo
+    def get_motivo(self):
+        return self.__motivo
+    def set_motivo(self,motivo):
+        self.__motivo = motivo
+    def get_cantidad(self):
+        return self.__cantidad
+    def set_cantidad(self,cantidad):
+        self.__cantidad = cantidad
+    def get_fecha(self):
+        return self.__fecha
+    def set_fecha(self,fecha):
+        self.__fecha = fecha
+    def get_stock_antes(self):
+        return self.__stock_antes
+    def get_stock_despues(self):
+        return self.__stock_despues
+    def get_monto_total(self):
+        return self.__monto_total
+    def set_monto_total(self,monto_total):
+        self.__monto_total = monto_total
+ 
+
 
     def __str__(self):
-        return (f"Movimiento ID: {self.__codigo_movimiento} | Producto: {self.__producto_codigo} |"
-                f"Tipo: {self.__tipo} | cantidad: {self.__cantidad} | Motivo: {self.__motivo} | "
-                f"Fecha: {self.__fecha.strftime('%Y-%m-%d %H:%M:%S')} | Usuario: {self.__usuario_responsable}")
+        return (f"[{self.__fecha}] {self.__tipo} | Codigo Producto: {self.__codigo_producto} | "
+                f"Empleado: {self.__usuario_responsable} | Cantidad: {self.__cantidad} | "
+                f"Motivo: {self.__motivo} | Stock antes/después: {self.__stock_antes}/{self.__stock_despues} | "
+                f"Monto Total: {self.__monto_total}")
 
 
-
-    def mostrar_movimientos(cls):
-        if not cls.movimientos_registrados:
-            print("No hay movimientos")
-        else:
-            for mov in cls.movimientos_registrados.values():
-                print(mov)
+  
